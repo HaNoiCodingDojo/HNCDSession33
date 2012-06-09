7dab6022b196f310042a609e43d286fa67ba7b10
Dương H. Nguyễn <cmpitg@gmail.com>
2012-06-09 14:40:23 +0700

    cmpitg

--- BinaryGenerator-2/test/BinaryGenerator_2/core_test.clj
+++ BinaryGenerator-2/test/BinaryGenerator_2/core_test.clj
@@ -2,5 +2,8 @@
   (:use clojure.test
         BinaryGenerator-2.core))
 
+(defn binary-generator [length]
+  [[]])
+
 (deftest test-when-n-is-0-return-list-contains-empty-sequence
   (is (= [[]] (binary-generator 0))))
