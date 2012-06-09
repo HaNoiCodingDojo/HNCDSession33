50779099dde897f74e73004e34f1b84eba76fa23
Dương H. Nguyễn <cmpitg@gmail.com>
2012-06-09 14:48:27 +0700

    huong

--- BinaryGenerator-2/test/BinaryGenerator_2/core_test.clj
+++ BinaryGenerator-2/test/BinaryGenerator_2/core_test.clj
@@ -10,3 +10,6 @@
 
 (deftest test-when-n-is-0-return-list-contains-empty-sequence
   (is (= [""] (binary-generator 0))))
+
+(deftest test-when-n-s-1-return-list-contains-0-and-1
+( is (= ["0" "1"] (binary-generator 1))))
