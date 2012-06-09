2c1ccb9acc82fced760a1da4f785404897492940
Dương H. Nguyễn <cmpitg@gmail.com>
2012-06-09 14:36:11 +0700

    cmpitg

---
+++ BinaryGenerator-2/src/BinaryGenerator_2/core.clj
@@ -0,0 +1,6 @@
+(ns BinaryGenerator-2.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ BinaryGenerator-2/test/BinaryGenerator_2/core_test.clj
@@ -0,0 +1,6 @@
+(ns BinaryGenerator-2.core-test
+  (:use clojure.test
+        BinaryGenerator-2.core))
+
+(deftest a-dummy-test
+  (is (= true true)))
