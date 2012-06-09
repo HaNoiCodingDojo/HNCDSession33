e2cf0ad33e8c72474b6db368742c18bbbb249afb
Dương H. Nguyễn <cmpitg@gmail.com>
2012-06-09 14:50:50 +0700

    huong

--- BinaryGenerator-2/test/BinaryGenerator_2/core_test.clj
+++ BinaryGenerator-2/test/BinaryGenerator_2/core_test.clj
@@ -6,10 +6,11 @@
 ;;; n = 2 -> ["00" "01" "10" "11"]
 
 (defn binary-generator [length]
-  [""])
+  (if (= length 0)  [""]
+      ["0" "1"]))
 
 (deftest test-when-n-is-0-return-list-contains-empty-sequence
   (is (= [""] (binary-generator 0))))
 
 (deftest test-when-n-s-1-return-list-contains-0-and-1
-( is (= ["0" "1"] (binary-generator 1))))
+  ( is (= ["0" "1"] (binary-generator 1))))
